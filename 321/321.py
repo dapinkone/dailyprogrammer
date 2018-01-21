7b3d45c730ba02c4bb92c6270058a34b1b89db41
DaPinkOne <crystalshade@gmail.com>
2018-01-20 22:15:00 -0500

    noticed forty was missing. I should write some tests...

--- 321/321.py
+++ 321/321.py
@@ -11,10 +11,10 @@ inputdata = """
 nums = {
         0: 'oh', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
         5: 'five', 6: 'six', 7: 'seven', 8: 'eight',  9: 'nine',
-        10: 'ten',  11: 'eleven', 12: 'twelve', 13: 'thirteen',
+        10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen',
         14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen',
         18: 'eighteen', 19: 'nineteen', 20: 'twenty',
-        30: 'thirty', 50: 'fifty', }
+        30: 'thirty', 40:'forty', 50: 'fifty', }
 
 
 def englishify(t):
